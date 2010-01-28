e2dfc8a0356ee7e065bd5ec8a2ca0f9b61c2daef
Ingo Molnar <mingo@elte.hu>
2010-01-28 07:00:25 +0100

    Revert "sched: cpuacct: Use bigger percpu counter batch values for stats counters"
    This reverts commit 43f85eab1411905afe5db510fbf9841b516e7e6a.
    
    build failure on UP.

--- kernel/sched.c
+++ kernel/sched.c
@@ -9026,7 +9026,6 @@ static void cpuacct_update_stats(struct task_struct *tsk,
 		enum cpuacct_stat_index idx, cputime_t val)
 {
 	struct cpuacct *ca;
-	int batch;
 
 	if (unlikely(!cpuacct_subsys.active))
 		return;
@@ -9034,9 +9033,8 @@ static void cpuacct_update_stats(struct task_struct *tsk,
 	rcu_read_lock();
 	ca = task_ca(tsk);
 
-	batch = min_t(long, percpu_counter_batch * cputime_one_jiffy, INT_MAX);
 	do {
-		__percpu_counter_add(&ca->cpustat[idx], val, batch);
+		percpu_counter_add(&ca->cpustat[idx], val);
 		ca = ca->parent;
 	} while (ca);
 	rcu_read_unlock();
